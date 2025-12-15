f290dae3bfbb63c9ea8b29a097d896d53dcd7ec0
kittu2217 <krishnamurthyghanta@gmail.com>
2025-12-15 17:19:33 +0000

    Add minimal unit test for QuantumInspiredLayer

--- tests/test_qil.py
+++ tests/test_qil.py
@@ -1,4 +1,10 @@
 
+import sys
+import os
+
+# Add project root to PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import torch
 from qinn.models.qil import QuantumInspiredLayer
 
